7e1afad92b120181071e9d6eb68ad51ed80ab80f
Alexandr G <alexandr@gavrisco.com>
2017-12-20 21:42:44 +0200

    Add products models (Category & Product)

--- axds/products/models.py
+++ axds/products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=128, db_index=True,
+    )
+    description = models.TextField()
+    
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'products.Category',
+        on_delete=models.CASCADE,
+    )
+
+    name = models.CharField(
+        max_length=128, db_index=True,
+    )
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    description = models.TextField()
+    
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
