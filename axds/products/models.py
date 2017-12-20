d70301602569dc374d0f042143aed6c1dd452f24
Alexandr G <alexandr@gavrisco.com>
2017-12-20 22:08:20 +0200

    Override str method for product/category. Make unique names for category

--- axds/products/models.py
+++ axds/products/models.py
@@ -4,9 +4,14 @@ from django.utils.translation import ugettext_lazy as _
 
 class Category(models.Model):
     name = models.CharField(
-        max_length=128, db_index=True,
+        max_length=128, unique=True,
     )
     description = models.TextField()
+
+    def __str__(self):
+        return _('[Category] %(name)s') % {
+            'name': self.name,
+        }
     
     class Meta:
         verbose_name = _('Category')
@@ -24,6 +29,12 @@ class Product(models.Model):
     )
     price = models.DecimalField(max_digits=12, decimal_places=2)
     description = models.TextField()
+
+    def __str__(self):
+        return _('[Product] %(name)s - %(price)s') % {
+            'name': self.name,
+            'price': self.price,
+        }
     
     class Meta:
         verbose_name = _('Product')
